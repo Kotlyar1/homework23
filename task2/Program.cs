98a6fc5de71812d19956a95ca5d2957ff1b96f7f
Александр Котляров <bony17@bk.ru>
2022-07-11 20:13:07 +0300

    Решил задачу 2

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите 2 числа,");
+Console.WriteLine("чтобы узнать какое большее");
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine("Число " + num1 + " большее");
+}
+else if (num1 == num2)
+{
+    Console.WriteLine("Число " + num1 + " равно числу " + num2);
+}
+else
+{
+    Console.WriteLine("Число " + num2 + " большее");
+}
