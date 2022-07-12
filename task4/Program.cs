2f54a019336091f5280b1394033726f8e4170353
Александр Котляров <bony17@bk.ru>
2022-07-12 17:08:59 +0300

    исправил программу теперь считает отрицательные и выводит максимальное

--- task4/Program.cs
+++ task4/Program.cs
@@ -6,8 +6,7 @@ Console.Write("Введите второе число и нажмите ENTER: "
 int num2 = int.Parse(Console.ReadLine());
 Console.Write("Введите третье число и нажмите ENTER: ");
 int num3 = int.Parse(Console.ReadLine());
-int maxNum = 0;
-if (num1 > maxNum) maxNum = num1;
+int maxNum = num1;
 if (num2 > maxNum) maxNum = num2;
 if (num3 > maxNum) maxNum = num3;
-Console.Write("Максимальное значение имеет число " + maxNum);
+Console.Write("Максимальное значение имеет число " + maxNum );
